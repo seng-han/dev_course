4539b9a9e6f804f83b5d61c4c2994f9abbdf246a
seng <kumjjick@icloud.com>
2024-12-15 00:51:46 +0900

    url prefix

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,15 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+
+@RequestMapping("/question")//URL의 프리픽스가 모두 /question으로 시작하니까 그냥 위로 빼줌
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
@@ -21,7 +24,7 @@ public class QuestionController {
 
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){// 숫자 2처럼 변하는 id값을 얻을 때에는 @pathVariable 사용
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);//addAttribute는 model객체에 데이터 추가
