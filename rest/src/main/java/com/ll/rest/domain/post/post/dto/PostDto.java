ee1d53fab0d0fa324d0fc3b6ed84a8f369b3df12
seng <kumjjick@icloud.com>
2024-12-19 10:51:45 +0900

    글 생성 data

--- rest/src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ rest/src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,16 +1,24 @@
 package com.ll.rest.domain.post.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ll.rest.domain.post.post.entity.Post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+//Dto는 외부를 상대함
 
 @Getter
 public class PostDto {
     private long id;
+
+    @JsonProperty("createdDatetime")
     private LocalDateTime createDate;
+
+    @JsonProperty("modifiedDate")
     private LocalDateTime modifyDate;
+
     private String title;
+
     private String content;
 
     public PostDto(Post post) {
--- rest/src/main/java/com/ll/rest/global/rsData/RsData.java
+++ rest/src/main/java/com/ll/rest/global/rsData/RsData.java
@@ -8,4 +8,12 @@ import lombok.Getter;
 public class RsData {
     private String resultCode;
     private String msg;
+    private long data; //생성할 때만 data가 보이게 해야함
+
+    public RsData(String resultCode, String msg){
+        //지금 rsdata에 세개인데 두개만 받는 것들을 위해서 생성자를 따로 만들어줌
+        this.resultCode = resultCode;
+        this.msg = msg;
+    }
 }
+//RsData 인자 두개받는 생성자, 세개받는 생성자 둘 다 만듦
