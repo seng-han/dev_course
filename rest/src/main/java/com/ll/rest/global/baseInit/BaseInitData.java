bebe51e590842d6a988d9dab4123803be2896ced
seng <kumjjick@icloud.com>
2024-12-20 12:21:52 +0900

    member package

--- rest/src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ rest/src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.ll.rest.global.baseInit;
 
+import com.ll.rest.domain.member.member.entity.Member;
+import com.ll.rest.domain.member.member.service.MemberService;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
+    private final MemberService memberService;
     private final PostService postService;
     @Autowired
     @Lazy
@@ -22,11 +25,23 @@ public class BaseInitData {
     public ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
     public void work1() {
+        if (memberService.count() > 0) return;
+
+        Member memberSystem = memberService.join("system", "1234", "시스템");
+        Member memberAdmin = memberService.join("admin", "1234", "관리자");
+        Member memberUser1 = memberService.join("user1", "1234", "유저1");
+        Member memberUser2 = memberService.join("user2", "1234", "유저2");
+        Member memberUser3 = memberService.join("user3", "1234", "유저3");
+    }
+
+    @Transactional
+    public void work2() {
         if (postService.count() > 0) return;
 
         Post post1 = postService.write("축구 하실 분?", "14시 까지 22명을 모아야 합니다.");
