80892d9b0716213fd24c6875ef4f4858f7a43fe6
seng <kumjjick@icloud.com>
2024-12-20 10:13:01 +0900

    error여러개 적용

--- rest/src/main/java/com/ll/rest/global/globalExceptionHandlers/GlobalExceptionHandler.java
+++ rest/src/main/java/com/ll/rest/global/globalExceptionHandlers/GlobalExceptionHandler.java
@@ -1,26 +1,47 @@
-package com.ll.rest.global.globalExceptionHandlers;
-
+package com.ll.rest.global.globalExceptionHandler;
 
 import com.ll.rest.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
-
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handle(NoSuchElementException ex){
+    public ResponseEntity<RsData<Void>> handle(NoSuchElementException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new RsData<>(
+                        "404-1",
+                        "해당 데이터가 존재하지 않습니다."
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new RsData<>(
                         "400-1",
-                        "해당 데이터가 존재하지 않습니다."
+                        message
                 ));
     }
 }
